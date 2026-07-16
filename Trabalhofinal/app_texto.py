194173cc134de0952f29eeaf807b2687c2d88dc0
gerferson394 <gerferson394@gmail.com>
2026-07-16 09:08:03 -0300

    Refactor main function to start text interface
    Updated comments for clarity and added function call to start the text interface.

--- Trabalhofinal/app_texto.py
+++ Trabalhofinal/app_texto.py
@@ -2,8 +2,8 @@ from controller.financeiro import GerenciadorFinanceiro
 from view_texto.menu import iniciar_interface_terminal
 
 if __name__ == "__main__":
-    # Inicializa o controlador (que carrega o arquivo JSON)
+    # inicia o controlador (que carrega o arquivo JSON)
     controlador = GerenciadorFinanceiro()
     
-    # Inicia a interface textual e entrega o controlador para ela
+    # inicia a interface textual 
     iniciar_interface_terminal(controlador)
